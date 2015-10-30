efee25d7ab9dcd4133c5a7d1659c22a0dd054f43
Alex Bosch <abosch19@gmail.com>
2015-10-30 18:08:48 +0100

    Practica 3 LP finalizada

--- Brain_3.py
+++ Brain_3.py
@@ -13,9 +13,33 @@ class WB(Brain):
         self.actions = QueueList.QueueList()
         self.pilot = Pilot.Pilot()
         self.backtrace = []
+        self.wumpus = True
         self.robot.move('reset')
     def step(self):
-        pass
-
+        if not self.robot.getItem('win'):
+            self.pilot.setSonar(self.robot.getItem('sonar'))
+            if self.pilot.isCrossRoad():
+                if self.wumpus:
+                    talk = self.robot.move('talk')
+                    if not talk == "This thing doesn't speak!":
+                        self.actions.push(talk)
+                    else:
+                        self.actions.moveHead()
+                        self.wumpus = False
+                        self.backtrace.append(self.inverse[self.pilot.getPrevious()])
+                        if self.pilot.moveTo(self.actions.getCurrent()):
+                            self.robot.move(self.actions.pop())
+                else:
+                    self.backtrace.append(self.inverse[self.pilot.getPrevious()])
+                    if self.pilot.moveTo(self.actions.getCurrent()):
+                        self.robot.move(self.actions.pop())
+                        
+            else:
+                if self.robot.move(self.pilot.nextMove()) == 'gold':
+                    self.robot.move('grab')
+                    self.backtrace.reverse()
+                    for i in self.backtrace:
+                        self.actions.push(i)
+                    
 def INIT(engine):
     return WB('WB', engine)
